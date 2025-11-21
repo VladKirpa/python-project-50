3633bd16ea4dfca37fe1cdb54286fde442b393f4
Vladislav Kirpa <vladkiirpa@gmail.com>
2025-11-21 21:56:31 +0300

    update gendiff -f --format

--- build/lib/gendiff/cli.py
+++ build/lib/gendiff/cli.py
@@ -7,8 +7,6 @@ def parser_cli():
 
     parser.add_argument('first_file')
     parser.add_argument('second_file')
-    parser.add_argument('-f', '--format', 
-                        help='set format of output')
 
     return parser.parse_args()
 
---
+++ build/lib/gendiff/scripts/gendiff.py
@@ -0,0 +1,13 @@
+from gendiff.cli import parser_cli
+
+
+def main():
+    res = parser_cli()
+    print(res)
+
+
+
+if __name__ == '__main__':
+    main()
+
+
