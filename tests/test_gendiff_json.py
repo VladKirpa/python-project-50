e1b17d11b732fc75542b576e37cde768f600a9a5
Vladislav Kirpa <vladkiirpa@gmail.com>
2025-11-27 22:56:08 +0300

    add gendiff_engine test

---
+++ tests/test_gendiff_json.py
@@ -0,0 +1,26 @@
+import os 
+import pytest
+from gendiff.scripts.gendiff_engine import generate_diff
+
+CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+def get_fixture_path(file_name):
+    return os.path.join(CURRENT_DIR, 'fixtures', file_name)
+
+def read_fixture(file_name):
+    with open(get_fixture_path(file_name), 'r') as f:
+        return f.read().strip()
+    
+
+def test_gendiff_json():
+
+    expected = read_fixture('expected_diff.txt')
+
+    file1 = get_fixture_path('file1.json')
+    file2 = get_fixture_path('file2.json')
+
+    result = generate_diff(file1, file2)
+
+    assert result.strip() == expected
+
+
