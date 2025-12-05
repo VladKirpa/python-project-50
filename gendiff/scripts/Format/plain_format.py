c3a6a69884024c7ef854b5ef0cf1a28928b946cf
Vladislav Kirpa <vladkiirpa@gmail.com>
2025-12-05 20:58:26 +0300

    make new logic with different format

---
+++ gendiff/scripts/Format/plain_format.py
@@ -0,0 +1,69 @@
+from gendiff.scripts.Format.stylish_format import to_str
+
+
+def is_value(value):
+
+    if isinstance(value, dict):
+        return "[complex value]"
+    elif isinstance(value, str):
+        return f"'{value}'"
+    else:
+        return to_str(value)
+
+        
+def get_path(data, path=[]):
+
+    for i in data:
+        
+        if i['status'] == 'nested':
+
+            path.append(i['key'])
+            for k in i['children']:
+                get_path(i['children'])
+
+        return path
+    
+
+def plain(data, path=""):
+
+    result = []
+
+    for i in data:
+        key = i["key"]
+        status = i["status"]
+
+        cur_path = f'{path}.{key}' if path else key
+        
+        if i["status"] == "nested":
+
+            child = plain(i['children'], cur_path)
+            result.extend(child)
+
+        else:
+
+            value = is_value(i["value"])
+
+            if status == 'added':
+                
+                result.append(
+                    f"Property '{cur_path}' was added with value: {value}")
+
+            elif status == 'deleted':
+
+                result.append(f"Property '{cur_path}' was removed")
+
+            elif status == 'changed':
+                
+                result.append(
+                    f"Property '{cur_path}' was updated. "
+                    f"From {is_value(i['old_value'])} to {value}")
+
+    return result
+
+
+                
+
+
+
+
+
---
+++ gendiff/scripts/Format/stylish_format.py
@@ -0,0 +1,75 @@
+
+def to_str(value):
+    if value is True:
+        return 'true'
+    elif value is False:
+        return 'false'
+    elif value is None:
+        return 'null'
+    return value
+
+
+###
+def stringify(value, deep):
+    space = ' ' * (deep * 4)
+    end_space = ' ' * ((deep * 4) - 4)
+
+    res = []
+    if isinstance(value, dict):
+        
+        for k, v in value.items():
+
+            val = stringify(v, deep + 1)
+            res.append(f"{space}{k}: {val}")
+
+            format = '\n'.join(res)
+
+            last_form = f'{{\n{format}\n{end_space}}}'
+            
+        return last_form
+            
+    else: 
+        return to_str(value)
+    
+
+###
+def stylish(data, deepth=1):
+    space = ' ' * ((deepth * 4) - 2)
+    end = ' ' * ((deepth * 4) - 2)
+
+    result = []
+    
+    for i in data:
+
+        if i['status'] == 'nested':
+            
+            result.append(f'{space}  {i['key']}: {{')
+            result.extend(stylish(i['children'], deepth + 1))
+            result.append(f'{end}  }}')
+
+        else:
+
+            key = i['key']
+            value = i['value']
+            status = i['status']
+
+            if status == 'added':
+
+                result.append(f"{space}+ {key}: {stringify(value, deepth + 1)}")
+            
+            elif status == 'deleted': 
+
+                result.append(f'{space}- {key}: {stringify(value, deepth + 1)}')
+
+            elif status == 'nonchanged':
+                
+                result.append(f'{space}  {key}: {stringify(value, deepth + 1)}')
+
+            elif status == 'changed':
+
+                old = i['old_value']
+
+                result.append(f'{space}- {key}: {stringify(old, deepth + 1)}')
+                result.append(f'{space}+ {key}: {stringify(value, deepth)}')
+        
+    return result
